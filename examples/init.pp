4fe48b95e744189d6f87521a41e80928afdff363
Chris Allen <chris.j.allen1979@gmail.com>
2025-11-18 12:26:28 +0000

    Automated conversion from geerlingguy.apache-php-fpm Ansible role

---
+++ examples/init.pp
@@ -0,0 +1,9 @@
+# Example usage of paw_ansible_role_apache_php_fpm
+
+# Simple include with default parameters
+include paw_ansible_role_apache_php_fpm
+
+# Or with custom parameters:
+# class { 'paw_ansible_role_apache_php_fpm':
+
+# }
